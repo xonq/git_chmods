fe8aea3f972f31544dff86eaf184abb4912ac30f
student <student@info-07.cshl.edu>
2022-10-23 20:17:18 -0400

    snake works almost

--- main/seq_snake/snake_test.py
+++ main/seq_snake/snake_test.py
@@ -4,30 +4,32 @@ import random
 
 WHITE = (255, 255, 255)
 green= (124, 252, 0)
+width=640 
+height=480
 
 class SpriteSheet(object):
     def __init__(self, file_name):
         self.file_name = file_name
-        self.sprite_sheet = pygame.image.load(file_name).convert()
+        self.sprite_sheet = pygame.image.load(file_name).convert_alpha()
     def get_image(self, x, y, width, height):
-        image = pygame.Surface([width, height]).convert()
+        image = pygame.Surface([width, height]).convert_alpha()
         image.blit(self.sprite_sheet, (0,0), (x, y, width, height))
         return image
 
 
-class Pore(pygame.sprite.Sprite):
+class Pore_up(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
         #make a list to hold your two sprite block
-        self.pore_Lshift = []
+        self.pore_up = []
 
         sprite_sheet = SpriteSheet('pore_sprite.png')
         print(f'fetching nanopores from {sprite_sheet}')
-        image = pygame.transform.scale(sprite_sheet.get_image(0, 0, 40, 750), [50, 1500])
-        self.pore_Lshift.append(image)
+        image = pygame.transform.scale(sprite_sheet.get_image(0, 0, 46, 375), [50, 1500])
+        self.pore_up.append(image)
         #initiate pore position
-        self.image = self.pore_Lshift[0]
-
+        self.image = self.pore_up[0]
+        
         self.top_boundary = 0
         self.bottom_boundary = 0
         self.left_boundary = 0
@@ -41,9 +43,35 @@ class Pore(pygame.sprite.Sprite):
 
         #edf block, change
         self.change_x = 0
+        print(self.pore_up)
 
+class Pore_down(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        #make a list to hold your two sprite block
+        self.pore_down = []
 
+        sprite_sheet = SpriteSheet('pore_sprite.png')
+        print(f'fetching nanopores from {sprite_sheet}')
+        image = pygame.transform.scale(sprite_sheet.get_image(0, 0, 46, 375), [50, 1500])
+        self.pore_down.append(pygame.transform.rotate(image, -180))
+        #initiate pore position
+        self.image = self.pore_down[0]
         
+        self.top_boundary = 0
+        self.bottom_boundary = 0
+        self.left_boundary = 0
+        self.right_boundary = 0
+        self.passed = False
+
+        #blit Pore(x, y) inputs at position in screen
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+        #edf block, change
+        self.change_x = 0
+        print(self.pore_down)
 
 
 class Player(pygame.sprite.Sprite):
@@ -111,13 +139,13 @@ class Player(pygame.sprite.Sprite):
             self.face_DN.append(pygame.transform.flip(obj, False, True))
        # scale up
         for obj in self.face_L:
-            self.face_L2.append(pygame.transform.scale(obj, [30, 30]))
+            self.face_L2.append(pygame.transform.scale(obj, [40, 40]))
         for obj in self.face_R:
-            self.face_R2.append(pygame.transform.scale(obj, [30, 30]))
+            self.face_R2.append(pygame.transform.scale(obj, [40, 40]))
         for obj in self.face_UP:
-            self.face_UP2.append(pygame.transform.scale(obj, [30, 30]))
+            self.face_UP2.append(pygame.transform.scale(obj, [40, 40]))
         for obj in self.face_DN:
-            self.face_DN2.append(pygame.transform.scale(obj, [30, 30]))
+            self.face_DN2.append(pygame.transform.scale(obj, [40, 40]))
         
         # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
@@ -128,13 +156,8 @@ class Player(pygame.sprite.Sprite):
         # Set speed vector
         self.change_x = 0
         self.change_y = 0
-        
-
-    def changespeed(self, x, y):
-        self.change_x += x
-        self.change_y += y
     
-    
+    #for each change in position, change:    
     def update(self):
         self.rect.x += self.change_x
         self.rect.y += self.change_y
@@ -150,14 +173,22 @@ class Player(pygame.sprite.Sprite):
         else:
             frame = (self.rect.y//30) % len(self.face_DN2)
             self.image = self.face_DN2[frame]
-        block_hit_list = pygame.sprite.spritecollide(self, Pore) #check self.pore
-        for block in block_hit_list:
-            if self.change_x > 0:
-                self.rect.right = block.rect.left
-            elif self.change_x < 0:
-                self.rect.left = block.rect.left
-
-
+        #keep on screen
+        if self.rect.x <0:
+            self.rect.x = 0
+        if self.rect.x > width -1:
+            self.rect.x = width -1
+        if self.rect.y <0:
+            self.rect.y = 0
+        if self.rect.y >= height - 1 :
+            self.rect.y = height -1 
+        
+        #sprite coordinates
+        sprite_coord_list = []
+        sprite_coord = ()
+        sprite_coord = (self.rect.x, self.rect.y)
+        sprite_coord_list.append(sprite_coord)
+        print(f'coordinates are {sprite_coord_list}')
 
 
     def go_left(self):
@@ -180,9 +211,23 @@ class Player(pygame.sprite.Sprite):
         self.change_x = 0
         self.change_y = 0
 
-        
+#Define class for dna trail
+class Trail(pygame.sprite.Sprite):
+    def __init__ (self, sprite):
+        super().__init__()
 
-#Define class for obstacles/pores
+        
+        
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+ 
+        # -- Attributes
+        # Set speed vector
+        self.change_x = 0
+        self.change_y = 0
+
+
 
 
 def main():
@@ -190,54 +235,73 @@ def main():
     pygame.init()
     print("initalizing")
 
+    width=640 
+    height=480
     #create screen
-    screen = pygame.display.set_mode([640, 480])
+    screen = pygame.display.set_mode([width, height])
  
     # Set the title of the window
     pygame.display.set_caption('python!')
+    background_image = pygame.image.load("landscape.png").convert_alpha()
+    background_image.set_alpha(10)
     
     #create Pore object with Pore(x,y) x, y = position on screen
     block_list = pygame.sprite.Group()
     all_sprites_list = pygame.sprite.Group()
+    list_x_coords = []
     print(f'#195 {block_list}, {type(block_list)}')
-    temp_int = 1
-    temp_int2 = 1
-    for i in [1,2,3]:
-        block = Pore(20, 20)
+    temp_int = 20
+    temp_int2 = 200
+    for i in [1,2]:
+        #define top block
+        block_up = Pore_up(20, 20)
+
+        #give x a random position 
+        block_up.rect.x=random.randrange(temp_int, temp_int2)
         
-        print(f'int_start is {temp_int}')
-        block.rect.x=random.randrange((20*(temp_int)), (170*(temp_int2)))
-        print(f'block rect x {block.rect.x}')
-        block.rect.y = random.randrange(-550, -300)
-        print(f'block rect y {block.rect.y}')
+        #capture x coord
+        x_coord = block_up.rect.x
+        print(f'x = {x_coord}')
+
+        #define y coord
+        block_up.rect.y = random.randrange(-1300, -1100)
+        y_coord = block_up.rect.y
+        print(f'y ={y_coord}')
+
+        block_down = Pore_down(20, 20)
+        block_down.rect.x = x_coord
+        block_down.rect.y = (y_coord+1300)
+        print(f'block_down.rect.y is {block_down.rect.y}')
+       
+        block_up.change_x = 300
+
+        #whole_pore.left_boundary = 0
+        #whole_pore.top_boundary = 0
+        #whole_pore.right_boundary = 480
+        #whole_pore.bottom_boundary = 400
+
+        #add to coordinate list
+        list_x_coords.append(x_coord)
+
+        block_list.add(block_up)
+        block_list.add(block_down)
+        all_sprites_list.add(block_up)
+        all_sprites_list.add(block_down)
         
-        block.change_x = random.randrange(block.rect.x, 600, 80)
-        print(f'x change {block.change_x}')
-        block.left_boundary = 0
-        block.top_boundary = 0
-        block.right_boundary = 480
-        block.bottom_boundary = 400
-
-        block_list.add(block)
-
-
         #all_sprites_list.add(block)
-        print(f'block_list {block_list}, all_sprite_list {all_sprites_list}')
-        temp_int2 += 1
-        temp_int += 5
-        print(f'int2_end is {temp_int}')
+        temp_int *= 10
+        temp_int2 *= 3.1
+        print(f'int_end is {temp_int}')
 
     # Create the player object
     
     all_sprites_list = pygame.sprite.Group()
-    #print(all_sprites_list)
-    #all_sprites_list.add(block)
     player = Player(0, 200)
     all_sprites_list.add(player)
     print(f'added player to all_sprites: {all_sprites_list}')
-    
-    #pore_image = Pore(10, 20)
 
+    #DNA object
+    nt_seq = []
 
     clock = pygame.time.Clock()
     done = False
@@ -262,6 +326,19 @@ def main():
         # Reset speed when key goes up
             if event.type == pygame.KEYUP:
                player.stop()
+            
+            
+
+            collisions = pygame.sprite.spritecollide(player, block_list, True)
+            if collisions:
+                print('oh no')
+            
+
+        #collisions
+        #define collision
+        
+        
+        #add an if for you removed all the blcoks
 
         #if pass through pore boundaries, add nt
  
@@ -272,13 +349,14 @@ def main():
  
     # -- Draw everything
     # background 
-        screen.fill(WHITE)
+        #screen.fill(WHITE)
  
     # Draw sprites
-        
-        block_list.draw(screen)
+        screen.blit(background_image, [0,0])
         all_sprites_list.draw(screen)
-       # screen.blit(pore_image[0], [20,20])
+        block_list.draw(screen)
+        
+        
  
     # Flip screen
         pygame.display.flip()

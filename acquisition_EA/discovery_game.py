b0bb31396846b54c52432b9eaa35113d47a76671
jmjacobs2 <jmjacobs2@gmail.com>
2022-10-23 21:48:33 -0400

    distorted grass but runs smoothly

--- acquisition_EA/discovery_game.py
+++ acquisition_EA/discovery_game.py
@@ -7,7 +7,6 @@ import sys
 import math
  
 # Global Variables
-COLOR = (255, 100, 98)
 x = 640
 y = 480
  
@@ -35,57 +34,86 @@ class Hero(pygame.sprite.Sprite):
         def moveBack(self, speed):
                 self.rect.y -= speed * speed/10
  
+class HideWildSeq(pygame.sprite.Sprite):
+        def __init__(self):
+                super().__init__()
+                self.image = pygame.image.load('images/grass.png')
+                self.x = 50
+                self.y = 220
+                self.width = 62
+                self.height = 64
+                self.image = pygame.transform.scale(self.image, (self.width, self.height))
+                self.rect = self.image.get_rect(center=(self.x, self.y))
+
+class HideBrokenSeq(pygame.sprite.Sprite):
+        def __init__(self):
+                super().__init__()
+                self.image = pygame.image.load('images/stage-seq2.png')
+                self.x = 480
+                self.y = 400
+                self.width = 68
+                self.height = 62
+                self.image = pygame.transform.scale(self.image, (self.width, self.height))
+                self.rect = self.image.get_rect(center=(self.x, self.y))
+
 class WildSeq(pygame.sprite.Sprite):
         def __init__(self):
                 super().__init__()
                 self.image = pygame.image.load('images/wildviralseq.png')
                 self.x = 50
                 self.y = 220
-                self.width = 100
-                self.height = 15
+                self.width = 400
+                self.height = 20
                 self.image = pygame.transform.scale(self.image, (self.width, self.height))
                 self.rect = self.image.get_rect(center=(self.x, self.y))
- 
+
 class BrokenSeq(pygame.sprite.Sprite):
         def __init__(self):
                 super().__init__()
                 self.image = pygame.image.load('images/wildbrokenviralseq.png')
                 self.x = 480
                 self.y = 400
-                self.width = 80
-                self.height = 15
+                self.width = 300
+                self.height = 20
                 self.image = pygame.transform.scale(self.image, (self.width, self.height))
                 self.rect = self.image.get_rect(center=(self.x, self.y))
   
-def main() 
-pygame.init()
-clock = pygame.time.Clock()
-size = (x, y)
-screen = pygame.display.set_mode(size)
-background =pygame.image.load('images/stage.png')
+def main(): 
+ pygame.init()
+ clock = pygame.time.Clock()
+ size = (x, y)
+ screen = pygame.display.set_mode(size)
+ background =pygame.image.load('images/stage.png')
 
 
-all_sprites_list = pygame.sprite.Group()
-hero = Hero()
-wildseq = WildSeq()
-wildbrokenseq = BrokenSeq()
-all_sprites_list.add(hero, wildseq, wildbrokenseq)
+ all_sprites_list = pygame.sprite.Group()
+ hero = Hero()
+ hiddenseq1 = HideWildSeq()
+ hiddenseq2 = HideBrokenSeq()
+ all_sprites_list.add(hero, hiddenseq1, hiddenseq2)
 
-def text_object(text, font):
-	textSurface = font.render(text, True, (0, 0, 0))
-	return textSurface, textSurface.get_rect()
-def message_display(text):
-	popuptext = pygame.font.SysFont('gilsans', 32)
-	TextSurf, TextRect = text_object(text, popuptext)
-	TextRect.center = ((x/2), (y/2))
-	screen.blit(TextSurf, TextRect)
+ wild_sprite_list = pygame.sprite.Group()
+ wildseq = WildSeq()
+ wild_sprite_list.add(wildseq)
+ wild_broken_sprite_list = pygame.sprite.Group()
+ wildbrokenseq = BrokenSeq()
+ wild_broken_sprite_list.add(wildbrokenseq)
 
-	pygame.display.update()
+ def text_object(text, font):
+        textSurface = font.render(text, True, (0, 0, 0))
+        return textSurface, textSurface.get_rect()
+ def message_display(text):
+        popuptext = pygame.font.SysFont('gilsans', 32)
+        TextSurf, TextRect = text_object(text, popuptext)
+        TextRect.center = ((x/2), (y/2))
+        screen.blit(TextSurf, TextRect)
+
+        pygame.display.update()
 	
  
-run = True
+ run = True
 
-while run:
+ while run:
         for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                         run = False
@@ -105,8 +133,11 @@ while run:
         if keys[pygame.K_UP]:
                 hero.moveBack(10)
 
-        seqfound = hero.rect.colliderect(wildseq.rect)
+        seqfound = hero.rect.colliderect(hiddenseq1.rect)
         if seqfound:
+                wild_sprite_list.draw(screen)
+                pygame.display.update()
+                pygame.time.delay(1000)
                 screen.fill((250, 250, 250))
                 message_display("You've found a wild sequence!")
                 pygame.time.delay(2000)
@@ -114,10 +145,12 @@ while run:
 
         brokenseqfound = hero.rect.colliderect(wildbrokenseq.rect)
         if brokenseqfound:
+                wild_broken_sprite_list.draw(screen)
+                pygame.display.update()
+                pygame.time.delay(1000)
                 screen.fill((250, 250, 250))
                 message_display("You've found a broken sequence, try again")
                 pygame.time.delay(2000)
-                play.again()                
 
 
         all_sprites_list.update()
@@ -128,7 +161,7 @@ while run:
         clock.tick(60)
 
 
-pygame.quit()
+ pygame.quit()
 
 if __name__ == "__main__":
 	main()

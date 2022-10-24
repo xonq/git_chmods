efbbbe3d7d7759e9b91783d4a6ba6ce375f11124
student <atvonsc@gmail.com>
2022-10-23 22:10:45 -0400

    adding

--- main/blast_module/blast_fighter/blast_fighter/blast_fighter_main.py
+++ main/blast_module/blast_fighter/blast_fighter/blast_fighter_main.py
@@ -2,6 +2,8 @@
 
 import pygame
 import subprocess
+import sys
+import time
 
 from pygame.surface import Surface, SurfaceType
 
@@ -25,9 +27,45 @@ YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 
-# load background image
-bg_image = pygame.image.load('blast_fighter/assets/images/background/8pixel_lab.jpg').convert_alpha()
+#define game variables
+#intro_count = 3
+#last_count_update = pygame.time.get_ticks()
+score = [0, 0] #player score
+round_over = False
+ROUND_OVER_COOLDOWN = 2000
+end_str = 'FATALITY'
 
+#define fighter variables
+WIZARD_SIZE = 250
+WIZARD_SCALE = 3
+WIZARD_OFFSET = [108, 107]
+WIZARD_DATA = [WIZARD_SIZE, WIZARD_SCALE, WIZARD_OFFSET]
+WARRIOR_SIZE = 150
+WARRIOR_SCALE = 3.5
+WARRIOR_OFFSET = [66, 45]
+WARRIOR_DATA = [WARRIOR_SIZE, WARRIOR_SCALE, WARRIOR_OFFSET]
+
+# load background image
+bg_image = pygame.image.load('/Users/student/PycharmProjects/pythonProject/git_chmods/main/blast_module/blast_fighter/assets/images/background/8pixel_lab.jpg').convert_alpha()
+
+#load spritesheets
+wizard_sheet = pygame.image.load('/Users/student/PycharmProjects/pythonProject/git_chmods/main/blast_module/blast_fighter/assets/images/wizard/Wizard_fin.png').convert_alpha()
+warrior_sheet = pygame.image.load('/Users/student/PycharmProjects/pythonProject/git_chmods/main/blast_module/blast_fighter/assets/images/warrior/warrior.png').convert_alpha()
+
+
+#define number of steps in each animation
+WIZARD_ANIMATION_STEPS = [8, 8, 2, 8, 8, 3, 7]
+WARRIOR_ANIMATION_STEPS = [8, 6, 4, 6, 6, 3, 7]
+
+#define fonts
+count_font = pygame.font.Font('/Users/student/PycharmProjects/pythonProject/git_chmods/main/blast_module/blast_fighter/assets/images/Font/Turok.ttf', 80)
+health_bar_font = pygame.font.Font('/Users/student/PycharmProjects/pythonProject/git_chmods/main/blast_module/blast_fighter/assets/images/Font/Turok.ttf', 30)
+end_font = pygame.font.Font('/Users/student/PycharmProjects/pythonProject/git_chmods/main/blast_module/blast_fighter/assets/images/Font/Turok.ttf', 80)
+
+#function for drawing text
+def draw_text(text, font, text_col, x, y):
+    img = font.render(text, True, text_col)
+    screen.blit(img, (x, y))
 
 # add a fxn for drawing background
 def draw_bg():
@@ -43,8 +81,8 @@ def draw_health_bar(health, x, y):
 
 
 # create 2 instances of fighters
-fighter_1 = Fighter(200, 0)
-fighter_2 = Fighter(700, 375)
+fighter_1 = Fighter(1, 200, 1, True, WIZARD_DATA, wizard_sheet, WIZARD_ANIMATION_STEPS)
+fighter_2 = Fighter(2, 700, 375, False, WARRIOR_DATA, warrior_sheet, WARRIOR_ANIMATION_STEPS)
 
 # create game loop
 count = 0
@@ -63,12 +101,41 @@ while run:
 
     #move fighters
     fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
-    #fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT)
+    fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
+
+    #update countdown
+    #if intro_count <= 0:
+     #   fighter_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_2)
+      #  fighter_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, fighter_1)
+    #else:
+        #display count timer
+     #   draw_text(str(intro_count), count_font, RED, SCREEN_WIDTH /2, SCREEN_HEIGHT / 3)
+        #update count timer
+      #  if (pygame.time.get_ticks() - last_count_update) >= 1000:
+       #     intro_count -= 1
+        #    last_count_update = pygame.time.get_ticks()
+
+
+    #update fighters
+    fighter_1.update()
+    fighter_2.update()
 
     # draw fighters
     fighter_1.draw(screen)
     fighter_2.draw(screen)
 
+    #check for player dead
+    if round_over == False:
+        if fighter_1.alive == False:
+            score[1] += 1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+        elif fighter_2.alive == False:
+            score[0] += 1
+            round_over = True
+            round_over_time = pygame.time.get_ticks()
+    else:
+        draw_text(end_str, end_font, RED, SCREEN_WIDTH/3, SCREEN_HEIGHT/3)
     # event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
--- main/blast_module/blast_fighter/blast_fighter/fighters.py
+++ main/blast_module/blast_fighter/blast_fighter/fighters.py
@@ -6,54 +6,110 @@ import random
 
 # make fighter class
 
-simonisms = ['You got me fucker', 'splendid', 'great form', 'keen for another round', 'wanker',
+simonisms = ['S H I T', 'Oof', 'great form', 'Yikes', 'wanker',
              'buy me a drink first', 'bloody hell']
 
 class Fighter():
-    def __init__(self, x, y):
-        self.flip = False
+    def __init__(self, player, x, y, flip, data, sprite_sheet, animation_steps):
+        self.player = player
+        self.size = data[0]
+        self.image_scale = data[1]
+        self.image_offset = data[2]
+        self.flip = flip
+        self.animation_list = self.load_images(sprite_sheet, animation_steps)
+        self.action = 0 # 0 = idle, 1 = run, 2 = jump, 3 = attack1, 4 = attack 2, 5 = hit, 6 = death
+        self.frame_index = 0
+        self.image = self.animation_list[self.action][self.frame_index]
+        self.update_time = pygame.time.get_ticks()
         self.rect = pygame.Rect((x, y, 80, 180))
         self.vel_y = 0
+        self.running = False
         self.jump = False
         self.attacking = False
         self.attack_type = 0
+        self.attack_cooldown = 0
+        self.hit = False
         self.health = 100
+        self.alive = True
 
+    def load_images(self, sprite_sheet, animation_steps):
+        #extract images from spritesheet
+        animation_list = []
+        for y, animation in enumerate(animation_steps):
+            temp_image_list = []
+            for x in range(animation):
+                temp_img = sprite_sheet.subsurface(x * self.size, y * self.size, self.size, self.size)
+                temp_image_list.append(pygame.transform.scale(temp_img, (self.size * self.image_scale, self.size * self.image_scale)))
+            animation_list.append(temp_image_list)
+
+        return animation_list
     def draw(self, surface):
+        img = pygame.transform.flip(self.image, self.flip, False)
         pygame.draw.rect(surface, (255, 0, 0), self.rect)
+        surface.blit(img, (self.rect.x - (self.image_offset[0] * self.image_scale), self.rect.y - (self.image_offset[1] * self.image_scale)))
 
     def move(self, screen_width, screen_height, surface, target):
         SPEED = 10
         GRAVITY = 2
         dx = 0
         dy = 0
+        self.running = False
+        self.attack_type = 0
 
         #get keypresses
         key = pygame.key.get_pressed()
 
         #can only perform other actions if only not attacking
-        if self.attacking == False:
+        if self.attacking == False and self.alive == True:
+            #check player 1 controls
+            if self.player == 1:
+            #movement
+                if key[pygame.K_a]:
+                    dx = -SPEED
+                    self.running = True
+                if key[pygame.K_d]:
+                    dx = SPEED
+                    self.running = True
 
-        #movement
-            if key[pygame.K_a]:
-                dx = -SPEED
-            if key[pygame.K_d]:
-                dx = SPEED
+                #jumping
+                if key[pygame.K_w] and self.jump == False:
+                    self.vel_y = -30
+                    self.jump = True
 
-            #jumping
-            if key[pygame.K_w] and self.jump == False:
-                self.vel_y = -30
-                self.jump = True
+                #attacks
+                if key[pygame.K_r] or key[pygame.K_t]:
+                    self.attack(surface, target)
 
-            #attacks
-            if key[pygame.K_r] or key[pygame.K_t]:
-                self.attack(surface, target)
+                    #which attack type is used
+                    if key[pygame.K_r]:
+                        self.attack_type = 1
+                    if key[pygame.K_t]:
+                        self.attack_type = 2
 
-                #which attack type is used
-                if key[pygame.K_r]:
-                    self.attack_type = 1
-                if key[pygame.K_t]:
-                    self.attack_type = 2
+            #check player 2 controls
+            if self.player == 2:
+            #movement
+                if key[pygame.K_LEFT]:
+                    dx = -SPEED
+                    self.running = True
+                if key[pygame.K_RIGHT]:
+                    dx = SPEED
+                    self.running = True
+
+                #jumping
+                if key[pygame.K_UP] and self.jump == False:
+                    self.vel_y = -30
+                    self.jump = True
+
+                #attacks
+                if key[pygame.K_KP1] or key[pygame.K_KP2]:
+                    self.attack(surface, target)
+
+                    #which attack type is used
+                    if key[pygame.K_KP1]:
+                        self.attack_type = 1
+                    if key[pygame.K_KP2]:
+                        self.attack_type = 2
 
             # apply gravity
             self.vel_y += GRAVITY
@@ -75,23 +131,78 @@ class Fighter():
             else:
                 self.flip = True
 
+            #apply attack cooldown
+            if self.attack_cooldown > 0:
+                self.attack_cooldown -= 1
 
             # update player position
             self.rect.x += dx
             self.rect.y += dy
 
+        #handle animation updates
+    def update(self):
+        #print(self.hit, self.attacking, self.health, self.jump, self.running, self.action)
+        #check what action is being performed
+        if self.health <= 0:
+            self.health = 0
+            self.alive = False
+            self.update_action(6) #death
+        elif self.hit == True:
+            self.update_action(5)
+        elif self.attacking == True:
+            if self.attack_type == 1:
+                self.update_action(3) # attack 1
+            elif self.attack_type == 2:
+                self.update_action(4) # attack 2
+        elif self.jump == True:
+            self.update_action(2) #jump
+        elif self.running == True:
+            self.update_action(1) # run
+        else:
+            self.update_action(0) #idle
+
+        animation_cooldown = 50
+        self.image = self.animation_list[self.action][self.frame_index]
+        if pygame.time.get_ticks() - self.update_time > animation_cooldown:
+            self.frame_index += 1
+            self.update_time = pygame.time.get_ticks()
+        if self.frame_index >= len(self.animation_list[self.action]):
+            self.frame_index = 0
+            #check if player is alive
+            if self.alive == False:
+                self.frame_index = len(self.animation_list[self.action]) - 1
+            else:
+                #check if an attack was executed
+                if self.action == 3 or self.action == 4:
+                    self.attacking = False
+                    self.attack_cooldown = 20
+                #check if damage was take
+                if self.action == 5:
+                    self.hit = False
+                    #if player in middle of attack, stop
+                    self.attacking = False
+                    self.attack_cooldown = 20
+
         #define attack method
-
     def attack(self, surface, target):
-        self.attacking = True
-        attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height) #makes attack surface 2x the player
-        if attacking_rect.colliderect(target.rect):
-            voice_cmd = f'say -v Daniel "{random.choice(simonisms)}"'
-            subprocess.Popen(voice_cmd, shell=True)
-            target.health -= 10
+        if self.attack_cooldown == 0:
+            self.attacking = True
+            attacking_rect = pygame.Rect(self.rect.centerx - (2 * self.rect.width * self.flip), self.rect.y, 2 * self.rect.width, self.rect.height) #makes attack surface 2x the player
+            if attacking_rect.colliderect(target.rect):
+                voice_cmd = f'say -v Daniel "{random.choice(simonisms)}"'
+                subprocess.Popen(voice_cmd, shell=True)
+                target.health -= 10
+                target.hit = True
 
-        pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
+            pygame.draw.rect(surface, (0, 255, 0), attacking_rect)
 
+    def update_action(self, new_action):
+        #check if new action is different than previous
+        if new_action != self.action:
+            self.action = new_action
+            #update animation settings
+            self.frame_index = 0
+            self.update_time = pygame.time.get_ticks()
 
 
 
